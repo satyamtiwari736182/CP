a88e24b3b873d915e19cc8f7c3f9726cea6d02f7
Satyam Tiwari <satyamtiwari@Satyams-MacBook-Air.local>
2025-11-30 08:39:25 +0530

    updated code

--- DataScience/sample2.py
+++ DataScience/sample2.py
@@ -19,3 +19,17 @@ def template():
 ''' As soon as python sees the @sample decorator above the template function, it immediately calls sample(template).
 # This means that "wrap function executed" is printed right away, and template is replaced by the wrapper function returned by sample.
 #? Later, when template() is called, it actually calls the wrapper function, which in turn calls the original template function. '''
+
+# ===========================================
+print('\n---------------------------')
+
+def simple_decorator(cls):
+    print(f"Decorating class: {cls.__name__}")
+    return cls
+
+@simple_decorator #! Class level decorator
+class MyClass:
+    pass
+
+if __name__ == "__main__":
+    pass
